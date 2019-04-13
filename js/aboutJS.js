a406ec84ed5e1ca7c6e58be9ff59bb3680810312
Roshan Paul <35750260+roshanpaul1@users.noreply.github.com>
2019-04-12 22:51:05 -0500

    adding javascripts

---
+++ js/aboutJS.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+	function insert(){
+            var src = document.getElementById("imge");
+            var img = document.createElement("img");
+            img.src = "media/imageISUD.webp";
+            src.appendChild(img);
+        }
+</script>
+		  
